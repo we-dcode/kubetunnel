f7ed08eb2ea6adb828ff18e5397017b8edd05844
tshaul <itamirshaul@gmail.com>
2022-09-30 00:21:00 +0000

    changed frps to kube-tunnel

--- garbage-collector/files/cleanup.sh
+++ garbage-collector/files/cleanup.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
   
-  charts_to_delete=$(kubectl get pods -l "frps=true" -o go-template='{{ range  $item := .items }}{{range .status.conditions }}{{ if (or (and (eq .type "PodScheduled") (eq .status "False")) (and (eq .type "Ready") (eq .status "False"))) }}{{index $item.metadata.labels "meta.helm.sh/release-name"}}{{"\n"}} {{ end }}{{ end }}{{ end }}' | grep -v "no value")
+  charts_to_delete=$(kubectl get pods -l "kube-tunnel=true" -o go-template='{{ range  $item := .items }}{{range .status.conditions }}{{ if (or (and (eq .type "PodScheduled") (eq .status "False")) (and (eq .type "Ready") (eq .status "False"))) }}{{index $item.metadata.labels "meta.helm.sh/release-name"}}{{"\n"}} {{ end }}{{ end }}{{ end }}' | grep -v "no value")
   
   if [ -n "$charts_to_delete" ]
   then
