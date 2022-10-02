cbdc68b4bdbbd5dc0e89481d65d1f0522b4f33d8
Eliran Wolff <eliranwolff@gmail.com>
2022-10-02 10:24:19 +0300

    changed version of helm releaser to v1.4.1

--- kube-tunnel/README.md
+++
@@ -1,6 +0,0 @@
-# kube-tunnel server
-
-## 
-There are multiple roles installed in the namespace:
-1. List/Get/Watch/Delete pods and deployments for garbage collection pod
-2. Patch services for frps pod
--- kube-tunnel/templates/health-cm.sh
+++
@@ -1,9 +0,0 @@
-apiVersion: v1
-kind: Secret
-metadata:
-  name: {{ include "kubetunnel.fullname" . }}-{{ .Values.env_service_name }}-health
-  labels:
-  {{- include "kubetunnel.labels" . | nindent 4 }}
-stringData:
-  health.sh: |
-    {{.Values.healthInline | nindent 4}}
